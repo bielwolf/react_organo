6c193c1e43ecf2d9e3beb2866c14397d60a700c7
bielwolf <gabryellobo@gmail.com>
2025-09-11 17:28:32 -0300

    trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const [colaboradores, setColaborador] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaborador([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="Data Science"/>
+      <Time nome="Devops"/>
+      <Time nome="UX e Design "/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
