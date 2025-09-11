c6c1dd8eafda99ec63e925f83f46bf0d2ca821c0
bielwolf <gabryellobo@gmail.com>
2025-09-11 01:53:41 -0300

    finalizacao da primeira parte

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+const Banner = () => {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da págia do Organo"/>
+        </header>
+    )
+}
+
+export default Banner
+
